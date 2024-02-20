3852bbe765a36d06a0c7ceb880f7738da4588af6
cristopher Quecholac <alum.2c@gmail.com>
2024-02-20 13:43:34 -0600

    exercise 3.5 fixed exercise 3.6

--- index.js
+++ index.js
@@ -41,13 +41,24 @@ app.get('/api/persons/:id', (request, response) => {
 })
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    if (!body.content) {
+    if (!body.name) {
         return response.status(400).json({
-            error: 'content missing'
+            error: 'name missing'
+        })
+    }
+    if(!body.number){
+        return response.status(400).json({
+            error:'number missing'
+        })
+    }
+    if(persons.find((person)=> person.name.toLowerCase()===body.name.toLowerCase())){
+        return response.status(400).json({
+            error: 'name must be unique'
         })
     }
     const person = {
-        content: body.content,
+        name: body.name,
+        number: body.number,
         id: generateId(100000000000000000000000000000000000000000000000000000000),
     }
     persons = persons.concat(person)
