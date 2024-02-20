f5720783f4f68f04652f5a71ca90e9c8ddd9bd51
cristopher Quecholac <alum.2c@gmail.com>
2024-02-20 13:16:47 -0600

    exercise 3.3

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ app.get('/', (request, response) => {
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(note => note.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
 app.get('/info', (request, response)=>{
     const date = new Date().toUTCString();
     const info =`
