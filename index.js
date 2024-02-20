db92de5821731f572263543a7637faa450bd7df6
cristopher Quecholac <alum.2c@gmail.com>
2024-02-20 13:14:26 -0600

    exercise 3.2

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ app.get('/', (request, response) => {
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
+app.get('/info', (request, response)=>{
+    const date = new Date().toUTCString();
+    const info =`
+        <p>phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>
+    `
+    response.send(info)
+})
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
