5dfcb69025904f152959f54b41bdc17ce7a8c912
cristopher Quecholac <alum.2c@gmail.com>
2024-02-22 10:19:05 -0600

    exercise 3.7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 let persons = [
     {
@@ -22,6 +23,7 @@ let persons = [
         "number": "39-23-6423122"
     }
 ]
+app.use(morgan('tiny'));
 const generateId = (max) => Math.floor(Math.random() * max);
 app.use(express.json())
 app.get('/', (request, response) => {
