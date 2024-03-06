72fbc005bd8ef0bd0ecf462f346b21f89ef8c400
cristopher Quecholac <alum.2c@gmail.com>
2024-03-06 15:12:16 -0600

    exercise 3.22

--- models/person.js
+++ models/person.js
@@ -3,34 +3,35 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(result => {
+    result
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        required: true
-    },
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true
+  },
 })
 personSchema.path('number').validate( v => {
-    if(/^(?=(?:\D*\d){8})\d{2,3}-\d{5,}/.test(v)===false){
-        throw new Error('format invalid');
-    }
-    return true;
+  if(/^(?=(?:\D*\d){8})\d{2,3}-\d{5,}/.test(v)===false){
+    throw new Error('format invalid')
+  }
+  return true
 })
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 module.exports = mongoose.model('Person', personSchema)
