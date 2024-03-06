8a33106a067fa79b88d8e0e5b77ef4c3bf95dc94
cristopher Quecholac <alum.2c@gmail.com>
2024-03-06 14:36:53 -0600

    exercises 3.19-3.20

--- index.js
+++ index.js
@@ -32,25 +32,15 @@ app.get('/api/persons/:id', (request, response, next) => {
         }
     }).catch(error => next(error))
 })
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-    if(!body.number){
-        return response.status(400).json({
-            error:'number missing'
-        })
-    }
     const person = new Person({
         name: body.name,
         number: body.number
     })
     person.save().then(savedPerson =>{
         response.json(savedPerson)
-    })
+    }).catch(error => next(error))
 })
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
@@ -63,17 +53,9 @@ app.delete('/api/persons/:id', (request, response, next) => {
         }).catch(error => next(error))
 })
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    const { name, number } = request.body
+    const opts = { new: true, runValidators: true, context: 'query' };
+    Person.findByIdAndUpdate(request.params.id, { name, number }, opts)
         .then(updatedPerson => {
             if (updatedPerson){
                 response.json(updatedPerson)
@@ -95,6 +77,8 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if(error.name === 'ValidationError') {
+        return response.status(400).send({ error: error.message })
     }
     next(error)
 }
--- models/person.js
+++ models/person.js
@@ -10,8 +10,21 @@ mongoose.connect(url)
         console.log('error connecting to MongoDB:', error.message)
     })
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        required: true
+    },
+})
+personSchema.path('number').validate( v => {
+    if(/^(?=(?:\D*\d){8})\d{2,3}-\d{5,}/.test(v)===false){
+        throw new Error('format invalid');
+    }
+    return true;
 })
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
